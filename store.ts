d785183d800b3b14f84c91cb8f94ce79158d88f3
Steven Rummler <steven.rummler@gmail.com>
2023-01-13 23:19:03 -0700

    reconfigure store

--- store.ts
+++ store.ts
@@ -4,51 +4,55 @@ import { entry, entryList } from './types';
 import _ from 'lodash';
 import dayjs from 'dayjs';
 
-const arrayOfFifteen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const arrayOfFifteen = Array(15)
+  .fill(0)
+  .map((e, i) => i);
 
 const slice = createSlice({
   name: 'data',
-  initialState: [
-    ...arrayOfFifteen.map(() => ({
-      entryType: 'food',
-      timestamp: dayjs()
-        .subtract(10 * 24 * 60 * Math.random(), 'minute')
-        .toJSON(),
-      number: 10 * _.round(10 + 40 * Math.random()),
-      ...(Math.random() > 0.5 && { label: 'Eggs' }),
-    })),
-    ...arrayOfFifteen.map(() => ({
-      entryType: 'active',
-      timestamp: dayjs()
-        .subtract(10 * 24 * 60 * Math.random(), 'minute')
-        .toJSON(),
-      number: 5 * _.round(100 * Math.random()),
-    })),
-    ...arrayOfFifteen.map(() => ({
-      entryType: 'weight',
-      timestamp: dayjs()
-        .subtract(10 * 24 * 60 * Math.random(), 'minute')
-        .toJSON(),
-      number: _.round(150 + 5 * Math.random(), 1),
-    })),
-  ] as entryList,
+  initialState: {
+    entries: [
+      ...arrayOfFifteen.map(() => ({
+        entryType: 'food',
+        timestamp: dayjs()
+          .subtract(10 * 24 * 60 * Math.random(), 'minute')
+          .toJSON(),
+        number: 10 * _.round(10 + 40 * Math.random()),
+        ...(Math.random() > 0.5 && { label: 'Eggs' }),
+      })),
+      ...arrayOfFifteen.map(() => ({
+        entryType: 'active',
+        timestamp: dayjs()
+          .subtract(10 * 24 * 60 * Math.random(), 'minute')
+          .toJSON(),
+        number: 5 * _.round(100 * Math.random()),
+      })),
+      ...arrayOfFifteen.map(() => ({
+        entryType: 'weight',
+        timestamp: dayjs()
+          .subtract(10 * 24 * 60 * Math.random(), 'minute')
+          .toJSON(),
+        number: _.round(150 + 5 * Math.random(), 1),
+      })),
+    ],
+  },
   reducers: {
-    addEntry: (state: entryList, action: { type: string; payload: entry }) => {
-      state.push(action.payload);
+    addEntry: (state, action: { type: string; payload: entry }) => {
+      state.entries.push(action.payload);
     },
-    removeEntry: (
-      state: entryList,
-      action: { type: string; payload: entry }
-    ) => {
-      state = state.filter((entry) => !_.isEqual(entry, action.payload));
+    removeEntry: (state, action: { type: string; payload: entry }) => {
+      const filteredEntries = state.entries.filter(
+        (entry) => !_.isEqual(entry, action.payload)
+      );
+      state.entries = filteredEntries;
     },
   },
 });
 
 const { addEntry, removeEntry } = slice.actions;
 
-function getEntries(state: { data: entryList }): entryList {
-  return state.data;
+function getEntries(state: { data: { entries: entryList } }): entryList {
+  return state.data.entries;
 }
 
 const reducer = combineReducers({
