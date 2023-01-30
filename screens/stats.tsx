8a7da2585e14e675479a6ba83d72efc9bccacde9
Steven Rummler <steven.rummler@gmail.com>
2023-01-29 20:31:34 -0700

    stats redesign

--- screens/stats.tsx
+++ screens/stats.tsx
@@ -1,12 +1,42 @@
 import CaloriesChart from '../components/caloriesChart';
-import { View } from 'react-native';
+import { OptionButton } from '../components/OptionButton';
+import Page from '../components/Page';
+import { Text } from 'react-native';
 import WeightChart from '../components/weightChart';
+import styled from 'styled-components/native';
+import { useState } from 'react';
+
+const charts = new Map(
+  Object.entries({
+    calories: <CaloriesChart />,
+    weight: <WeightChart />,
+  })
+);
 
 export default function StatsScreen() {
+  const [chart, setChart] = useState<string>('calories');
   return (
-    <View>
-      <WeightChart />
-      <CaloriesChart />
-    </View>
+    <Page style={{ paddingTop: 90 }}>
+      <OptionsSection>
+        <OptionButton onPress={() => setChart('calories')}>
+          <Text style={chart === 'calories' ? {} : { color: 'lightgrey' }}>
+            Calories
+          </Text>
+        </OptionButton>
+        <OptionButton onPress={() => setChart('weight')}>
+          <Text style={chart === 'weight' ? {} : { color: 'lightgrey' }}>
+            Weight
+          </Text>
+        </OptionButton>
+      </OptionsSection>
+      {charts.get(chart)}
+    </Page>
   );
 }
+
+const OptionsSection = styled.View`
+  flex: 1 1 0;
+  max-height: 133px;
+  flex-direction: row;
+  padding: 0px 10px 0px 10px;
+`;
