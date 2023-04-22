65fce933dba4b61457525b31b2384fbd282a23cc
Steven Rummler <steven.rummler@gmail.com>
2023-04-22 17:06:40 -0600

    added sentry to debug, updated dependencies

--- App.tsx
+++ App.tsx
@@ -6,6 +6,14 @@ import Navigator from './Navigator';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://7defc0a9e6a14cf98c568b8b9b8b451e@o4505059880468480.ingest.sentry.io/4505059883876352',
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
 export default function App() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   setCustomText({
