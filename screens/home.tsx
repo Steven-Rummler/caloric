1ee697c44a39ac743eda72564fc51dde94f62272
Steven Rummler <steven.rummler@gmail.com>
2023-08-13 16:27:46 -0600

    updated home page stats

--- screens/home.tsx
+++ screens/home.tsx
@@ -28,20 +28,12 @@ export default function HomeScreen({ navigation }: Props) {
     () => computeRecentFoodCalories(entries),
     [entries]
   );
-  const averageFoodCalories = useMemo(
-    () => computeAverageFoodCalories(entries),
-    [entries]
-  );
-  const weeklyWeightChange = _.round(
-    (7 * (averageFoodCalories + passiveCalories)) / 3500,
-    1
-  );
-  const monthlyWeightChange = _.round(
-    ((365 / 12) * (averageFoodCalories + passiveCalories)) / 3500,
-    1
-  );
   const totalWeightChange = useMemo(
     () => computeTotalWeightChange(entries, passiveCalories),
+    [entries, passiveCalories]
+  );
+  const currentWeight = useMemo(
+    () => computeCurrentWeight(entries, passiveCalories),
     [entries, passiveCalories]
   );
 
@@ -53,24 +45,16 @@ export default function HomeScreen({ navigation }: Props) {
       <InfoSection>
         <InfoColumn>
           <InfoCard label="Calories Today" value={`${recentFoodCalories}`} />
-          <InfoCard label="Average Calories" value={`${averageFoodCalories}`} />
           <InfoCard
-            label="Average Burned"
+            label="Average Calories Burned"
             value={`${Math.round(-1 * passiveCalories)}`}
           />
         </InfoColumn>
         <InfoColumn>
+          <InfoCard label="Current Weight" value={`${currentWeight.toFixed(1)}`} />
           <InfoCard
             label={`Weight ${totalWeightChange > 0 ? 'Gained' : 'Lost'}`}
-            value={`${Math.abs(totalWeightChange)}lbs`}
-          />
-          <InfoCard
-            label={`Weight ${weeklyWeightChange > 0 ? 'Gain' : 'Loss'} per Week`}
-            value={`${Math.abs(weeklyWeightChange)}lbs`}
-          />
-          <InfoCard
-            label={`Weight ${monthlyWeightChange > 0 ? 'Gain' : 'Loss'} per Month`}
-            value={`${Math.abs(monthlyWeightChange)}lbs`}
+            value={`${Math.abs(totalWeightChange).toFixed(1)}`}
           />
         </InfoColumn>
       </InfoSection>
@@ -101,6 +85,7 @@ const InfoSection = styled.View`
 `;
 const InfoColumn = styled.View`
   flex: 1 1 0;
+  justify-content: space-evenly;
 `;
 const ActionSection = styled.View`
   flex: 5 1 0;
@@ -125,7 +110,7 @@ const InfoLabel = styled.Text`
   text-align: center;
 `;
 const InfoValue = styled.Text`
-  font-size: 16px;
+  font-size: 18px;
   text-align: center;
 `;
 
@@ -141,22 +126,6 @@ function computeRecentFoodCalories(entries: entry[]) {
     0
   );
   return Math.round(recentFoodCalories);
-}
-
-function computeAverageFoodCalories(entries: entry[]) {
-  const foodEntries = entries.filter((entry) => entry.entryType === 'food');
-  if (foodEntries.length === 0) return 0;
-  const totalFoodCalories = foodEntries.reduce(
-    (total, next) => total + next.number,
-    0
-  );
-  const days: string[] = [];
-  foodEntries.forEach((foodEntry) => {
-    const day = dayjs(foodEntry.timestamp).format('YYYY-MM-DD');
-    if (days.includes(day)) return;
-    days.push(day);
-  });
-  return Math.round(totalFoodCalories / days.length);
 }
 
 function computeTotalWeightChange(entries: entry[], passiveCalories: number) {
@@ -179,4 +148,23 @@ function computeTotalWeightChange(entries: entry[], passiveCalories: number) {
     weightSeries[weightSeries.length - 1].y - weightSeries[0].y,
     1
   );
+}
+
+function computeCurrentWeight(entries: entry[], passiveCalories: number) {
+  const weightData = entries
+    .filter((entry) => entry.entryType === 'weight')
+    .map((e) => ({
+      x: dayjs(e.timestamp).toDate(),
+      y: e.number,
+    }));
+
+  const weightSeries = computeActualWeightSeries(
+    entries,
+    weightData,
+    passiveCalories
+  );
+
+  if (weightSeries.length === 0) return 0;
+
+  return _.round(weightSeries[weightSeries.length - 1].y, 1);
 }
