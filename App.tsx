f91b1591fed444e9789f429546c3724d059cdbaf
Steven Rummler <steven.rummler@gmail.com>
2025-11-04 10:21:46 -0700

    remove sentry

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './store';
 import Navigator from './Navigator';
 
-Sentry.init({
-  dsn: 'https://7defc0a9e6a14cf98c568b8b9b8b451e@o4505059880468480.ingest.sentry.io/4505059883876352',
-  debug: true,
-  tracesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: 'https://7defc0a9e6a14cf98c568b8b9b8b451e@o4505059880468480.ingest.sentry.io/4505059883876352',
+//   debug: true,
+//   tracesSampleRate: 1.0,
+// });
 
 export default function App() {
   return (
